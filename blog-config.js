89c8501bd92002fc4c09db5327be94ddc56036e6
changhyeon <changhyeon.h@kakao.com>
2024-01-14 20:07:17 +0900

    docs: base url fix

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "황창구리의 개발 블로그",
   description: "함정 카드가 없는 개발자가 되기 위해 노력하고 있습니다",
   author: "황창현",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://hchanghyeon.github.io/dev-blog/",
   links: {
     github: "https://github.com/Hchanghyeon/",
     linkedIn: "https://www.linkedin.com/in/%EC%B0%BD%ED%98%84-%ED%99%A9-3a9433267/",
