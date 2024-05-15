dd621899cd5e192b5eb5345c3df5ea4478bbcd2e
changhyeon <changhyeon.h@kakao.com>
2024-05-15 15:10:56 +0900

    docs: 이력서 주소 변경

--- blog-config.js
+++ blog-config.js
@@ -7,7 +7,7 @@ module.exports = {
     github: "https://github.com/Hchanghyeon/",
     linkedIn: "https://www.linkedin.com/in/%EC%B0%BD%ED%98%84-%ED%99%A9-3a9433267/",
     email: "mailto:changhyeon.h@kakao.com",
-    resume: "https://changhyeonhw.notion.site/3bf02fb38ead439eb3d2768c118e47a9",
+    resume: "https://docs.google.com/document/d/1DZwqAEXqHqtVyy6PAt5TgamKgIqiRkDCocXBpwJ3lfo/edit?usp=sharing",
   },
   utterances: {
     repo: "Hchanghyeon/dev-blog",
